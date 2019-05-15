a3d4923517f6dc0915850c53742d320a218ce67b
Ben Reynolds-Carr <bjr.kitz@gmail.com>
2019-05-15 16:35:42 +0100

    player 2 now has hit points

--- spec/features/player_hp_spec.rb
+++ spec/features/player_hp_spec.rb
@@ -1,10 +1,10 @@
-feature 'Names' do
-  scenario 'players enter two names' do
+feature "Hit Points" do
+  scenario "player 2's hit points are shown" do
     visit('/')
     fill_in('player1', with:'Alice')
     fill_in('player2', with:'Bob')
     click_button('Submit')
 
-    expect(page).to have_text('Alice vs. Bob')
+    expect(page).to have_text("Bob: 100HP")
   end
 end
