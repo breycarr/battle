d7131ffd7346faf1099958319521d34c7e7aa46b
Ben Reynolds-Carr <bjr.kitz@gmail.com>
2019-05-15 16:02:40 +0100

    refactored to redirect names to play

--- app.rb
+++ app.rb
@@ -3,15 +3,22 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
-      'Testing infrastructure working!'
       erb(:index)
   end
 
   post '/names' do
-    p params
-    @player1 = params[:first_name]
-    @player2 = params[:second_name]
-    erb(:names)
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
+    erb(:play)
   end
 end
--- spec/features/players_name_spec.rb
+++ spec/features/players_name_spec.rb
@@ -1,8 +1,8 @@
 feature 'get names' do
   scenario 'players enter two names' do
     visit('/')
-    fill_in('first_name', with:'Alice')
-    fill_in('second_name', with:'Bob')
+    fill_in('player1', with:'Alice')
+    fill_in('player2', with:'Bob')
     click_button('Submit')
 
     expect(page).to have_text('Alice vs. Bob')
