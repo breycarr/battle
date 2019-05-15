ab0fb4c0e59d11bffaabc48a71292fced2275200
Ben Reynolds-Carr <bjr.kitz@gmail.com>
2019-05-15 16:46:43 +0100

    web helpers implemented

--- spec/features/player_hp_spec.rb
+++ spec/features/player_hp_spec.rb
@@ -1,10 +1,6 @@
 feature "Hit Points" do
   scenario "player 2's hit points are shown" do
-    visit('/')
-    fill_in('player1', with:'Alice')
-    fill_in('player2', with:'Bob')
-    click_button('Submit')
-
+    sign_in_and_play
     expect(page).to have_text("Bob: 100HP")
   end
 end
--- spec/features/players_name_spec.rb
+++ spec/features/players_name_spec.rb
@@ -1,10 +1,6 @@
 feature 'Names' do
   scenario 'players enter two names' do
-    visit('/')
-    fill_in('player1', with:'Alice')
-    fill_in('player2', with:'Bob')
-    click_button('Submit')
-
+    sign_in_and_play
     expect(page).to have_text('Alice vs. Bob')
   end
 end
